5c6b62bc55ba320dbecfc9e36ab49a5fb12bb1de
innomon <tech@innomon.in>
2026-03-22 05:55:05 +0530

    wi[

--- framework/init.go
+++ framework/init.go
@@ -15,7 +15,7 @@ import (
 	"github.com/formcms/formcms-go/core/services"
 	"github.com/formcms/formcms-go/infrastructure/filestore"
 	"github.com/formcms/formcms-go/infrastructure/relationdbdao"
-	"github.com/go-chi/chi/v5"
+	chi "github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"golang.org/x/crypto/acme/autocert"
 )
